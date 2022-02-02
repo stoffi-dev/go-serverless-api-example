f0e6d82dbefc3faaea84e588bb82409bf7a4ec99
chris <cg@xps-13>
2022-02-02 17:27:36 +1000

    updated main

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,7 @@ import(
    "os"
    "github.com/aws/aws-lambda-go/events"
    "github.com/aws/aws-lambda-go/lambda"
-   "github.com/aws-sdk-go/aws"
+   "github.com/aws/aws-sdk-go"
    "github.com/aws/aws-sdk-go/aws/session"
    "github.com/aws/aws-sdk-go/service/dynamodb"
 )
